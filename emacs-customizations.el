5fbb931d32f53949d537a3196e748fdd16c37cbd
Hector Bavio <hectorbavio@gmail.com>
2011-12-14 18:02:34 -0300

    emacs reboot #6

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mark-even-if-inactive nil)
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
+ '(package-enable-at-startup t)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
