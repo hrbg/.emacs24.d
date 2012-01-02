ddfeb36df50561de926afcccfa3612932a3d1d6d
Hector Bavio <hectorbavio@gmail.com>
2012-01-02 13:48:37 -0300

    Color theme

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -4,6 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(custom-theme-load-path (quote (custom-theme-directory t "/home/hector/.emacs24.d/elisp/external/")))
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
  '(mark-even-if-inactive nil)
@@ -16,4 +17,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(font-lock-type-face ((t (:foreground "#666" :slant normal :weight normal :height 200 :width normal :foundry "unknown" :family "Inconsolata")))))
---
+++ init.d/color-theme.el
@@ -0,0 +1,3 @@
+(require 'color-theme)
+(load-file "~/.emacs24.d/elisp/external/color-theme-solarized/solarized-light-theme.el")
+;; (color-theme-railscasts)
