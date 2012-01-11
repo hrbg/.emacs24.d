5a35866621acc4b51daccd1b97e2b7f5cb7d27ca
Hector Bavio <hectorbavio@gmail.com>
2012-01-11 19:17:30 -0300

    Comment or un comment region or line Flymake Coffee

--- init.d/code.el
+++ init.d/code.el
@@ -5,3 +5,26 @@
 ;; Smart tab
 (require 'smart-tab)
 (global-smart-tab-mode 1)
+
+;; Coffescript indent with 2 spaces
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+
+;; http://pablo.rauzy.name/init.el.html
+;; comment-or-uncomment-region-or-line
+(defun comment-or-uncomment-region-or-line ()
+  "Like comment-or-uncomment-region, but if there's no mark \(that means no
+region\) apply comment-or-uncomment to the current line"
+  (interactive)
+  (if (not mark-active)
+      (comment-or-uncomment-region
+       (line-beginning-position) (line-end-position))
+    (if (< (point) (mark))
+        (comment-or-uncomment-region (point) (mark))
+      (comment-or-uncomment-region (mark) (point)))))
+(global-set-key (kbd "C-/") 'comment-or-uncomment-region-or-line)
--- init.d/flymake.el
+++ init.d/flymake.el
@@ -1,2 +1,5 @@
 (require 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+
+
+(add-hook 'coffee-mode-hook 'flymake-coffee-load)
