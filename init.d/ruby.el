df27f3a6d51d8b6fad07589db10927241753dda2
Hector Bavio <hectorbavio@gmail.com>
2012-01-13 11:03:04 -0300

    Add ruby interpolation Gemfile are ruby files too

---
+++ init.d/ruby.el
@@ -0,0 +1,16 @@
+;; Interpolation
+(defun ruby-interpolate ()
+  "In a double quoted string, interpolate."
+  (interactive)
+  (insert "#")
+  (when (and
+         (looking-back "\".*")
+         (looking-at ".*\""))
+    (insert "{}")
+    (backward-char 1)))
+
+(define-key ruby-mode-map (kbd "#") 'ruby-interpolate)
+
+;; Ruby mode hooks
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile\\'" . ruby-mode))
